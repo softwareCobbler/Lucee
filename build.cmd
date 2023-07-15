18e8cf747875a9130e7eef6894e6abd9708a35bc
David Rogers <davidwrogers2@gmail.com>
2023-07-15 08:08:22 -0500

    build stuff

---
+++ build.cmd
@@ -0,0 +1,6 @@
+@echo off
+setlocal
+set JAVA_HOME=C:\Program Files\Java\jdk-11.0.2
+cd loader
+ant fast
+endlocal
---
+++ install.cmd
@@ -0,0 +1,3 @@
+del /F /Q ..\docker\bind-mount\lucee\*
+copy loader\target\*.lco ..\docker\bind-mount\lucee\
+copy loader\target\*.jar ..\docker\bind-mount\lucee\
